c24b7882e58086e71de0d735623641ecf9a41129
Toni  Diep <adiep123@gmail.com>
2020-06-16 20:54:02 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
-  def change_column
+  def change
     # (table_name, column_name, type)
     change_column :students, :birthdate, :datetime
 
